6384c724b83e3b418f9ab22a11b8fa1e673f05db
Sai Keerthan Achugatla <424103@student.nitandhra.ac.in>
2026-08-11 14:33:52 +0530

    Valid Parentheses

---
+++ lc20.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto ch: s){
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }else if(st.empty()){
+                    return false;
+                }
+            
+            else{
+                char top=st.top();
+                if((ch==')'&&top=='(')||(ch=='}'&&top=='{')||ch==']'&&top=='['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.size()==0;
+    }
+};
