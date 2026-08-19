5ab2ae10eb525722b956c88b95ebf8f10b7d15d8
Sai Keerthan Achugatla <424103@student.nitandhra.ac.in>
2026-08-20 02:20:36 +0530

    Move all balls to given index (use Prefix and sufffix sum for O(n)

---
+++ lc1769.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n = boxes.size();
+        vector<int> ans(n), left(n, 0), right(n, 0);
+        int count;
+        count = (boxes[0] == '1' ? 1 : 0);
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] + count;
+            count += (boxes[i] == '1' ? 1 : 0);
+        }
+        count = (boxes[n - 1] == '1' ? 1 : 0);
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] + count;
+            count += (boxes[i] == '1' ? 1 : 0);
+        }
+        for (int i = 0; i < n; i++) {
+            ans[i] = left[i] + right[i];
+        }
+        return ans;
+    }
+};
